699f3cc8fc457c95ef557afcd79a4d6f2e6c0234
duy123a <duy123a@gmail.com>
2023-02-12 21:07:39 +0700

    update meta tag

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,4 +1,4 @@
-import imageUrl from '@/assets/dashboard.png';
+import imageUrl from '@/assets/dashboard.jpg';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import Box, { BoxProps } from '@mui/material/Box';
